1d08cd4230c38a6214be82a6a33d212c6cde3077
Oxi_Cuza <oxicusa@gmail.com>
2021-01-24 15:39:59 +0700

    seeding user admin

---
+++ database/seeders/AdministratorSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class AdministratorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = new \App\Models\User;
+        
+        $admin->username = 'administrator';
+        $admin->name = 'Site Administrator';
+        $admin->email = 'admin@larashop.test';
+        $admin->roles = json_encode(['ADMIN']);
+        $admin->password = \Hash::make('larashop');
+        $admin->avatar = 'tidak-ada-gambar.png';
+        $admin->address = 'Malang, Jawa Timur';
+
+        $admin->save();
+        $this->command->info('User Admin berhasil ditambah !');
+    }
+}
