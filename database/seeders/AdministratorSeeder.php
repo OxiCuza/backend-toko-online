55914519d3e271081aaf829f2b1729169152da01
Oxi_Cuza <oxicusa@gmail.com>
2021-05-23 21:59:32 +0700

    update db seeder

--- database/seeders/AdministratorSeeder.php
+++ database/seeders/AdministratorSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class AdministratorSeeder extends Seeder
 {
@@ -14,12 +15,12 @@ class AdministratorSeeder extends Seeder
     public function run()
     {
         $admin = new \App\Models\User;
-        
+
         $admin->username = 'administrator';
         $admin->name = 'Site Administrator';
         $admin->email = 'admin@larashop.test';
         $admin->roles = json_encode(['ADMIN']);
-        $admin->password = \Hash::make('larashop');
+        $admin->password = Hash::make('larashop');
         $admin->avatar = 'tidak-ada-gambar.png';
         $admin->address = 'Malang, Jawa Timur';
 
